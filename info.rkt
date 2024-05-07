2f84d13a2d08accf2bf3d269e90b66f9522177f2
Ray Miller <ray@metail.co.uk>
2024-05-07 17:11:22 +0100

    Add info file

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "racket-utils")
+(define deps '("base" "http-easy-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/tryme.scrbl" ())))
+(define pkg-desc "Some utilities for working with racket")
+(define version "0.0")
+(define pkg-authors '(ray1729))
+(define license '(GPL-3.0-only))
