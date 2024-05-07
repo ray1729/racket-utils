49f5b85e22d349e03f44da24571ae53e45bb1d07
Ray Miller <ray@metail.co.uk>
2024-05-07 17:14:20 +0100

    Remove scribblings ref

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "racket-utils")
 (define deps '("base" "http-easy-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/tryme.scrbl" ())))
+;;(define scribblings '(("scribblings/tryme.scrbl" ())))
 (define pkg-desc "Some utilities for working with racket")
 (define version "0.0")
 (define pkg-authors '(ray1729))
